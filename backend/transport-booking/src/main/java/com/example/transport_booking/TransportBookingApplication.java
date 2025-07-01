3dfceb011e999cae3a3edfc7e2692bc2ace0693c
Varel Moudio <varelmoudio14@gmail.com>
2025-07-01 11:33:52 +0100

    added spring boot backend

---
+++ backend/transport-booking/src/main/java/com/example/transport_booking/TransportBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.transport_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportBookingApplication.class, args);
+	}
+
+}
---
+++ backend/transport-booking/src/test/java/com/example/transport_booking/TransportBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transport_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
